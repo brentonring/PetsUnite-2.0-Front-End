96e2135baee87e41a17b68a275dedcef3bc300d0
Brenton Ring <97139815+brentonring@users.noreply.github.com>
2022-07-23 12:11:20 -0600

    edited add page components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomeBody from './components/HomeBody'
 import Footer from './components/Footer'
 import ServiceIndex from './components/ServiceIndex';
 import AdoptionIndex from './components/AdoptionIndex';
+import NewAdoption from './components/NewAdoption';
 // import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 // import error404 from './components/error404'
@@ -13,11 +14,22 @@ function App() {
   return (
     <div>
       <Nav />
+<<<<<<< Updated upstream
       {/* <HomeBody /> */}
       <AdoptionIndex />
       {/* <ServiceIndex /> */}
       {/* <EventIndex /> */}
       <Footer />
+=======
+      <HomeBody />
+      {/* <AdoptionIndex />
+      <ServiceIndex />
+      <EventIndex /> */}
+      <NewAdoption />
+      <Footer />
+      
+      
+>>>>>>> Stashed changes
     </div>
   );
 }
